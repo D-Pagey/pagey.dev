247058b211c6850362f5f5e5ed042b89e99b6772
Dan Page <dan.page91@gmail.com>
2019-07-14 01:22:36 +0100

    build(storybook): add and configure storybook

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { addDecorator, configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+addDecorator((storyFn) => <div style={{ margin: '1rem' }}>{storyFn()}</div>);
+
+configure(() => {
+    const req = requireContext('../src', true, /\.stories\.js$/);
+    req.keys().forEach((filename) => req(filename));
+}, module);
