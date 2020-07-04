95e0666b12004828ea03e950bfdc4b66682fe71b
Andy Huynh <36887000+andy-h1@users.noreply.github.com>
2020-07-04 19:38:32 +0100

    Add Tracker Component

--- src/components/PageTracker/index.js
+++ src/components/PageTracker/index.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { format } from 'date-fns';
+import { Tracker } from './Tracker';
 
 export const PageTracker = () => {
     const [andyData, setAndyData] = useState();
-    const [joeData, setJoeData] = useState();
+    const [huntData, setHuntData] = useState();
 
+    // fetch github data
     useEffect(() => {
         async function fetchData() {
             // You can await here
@@ -25,7 +26,7 @@ export const PageTracker = () => {
             // ...
             const pushEvents = data.filter((event) => event.type === 'PushEvent');
 
-            setJoeData(pushEvents.slice(0, 3));
+            setHuntData(pushEvents.slice(0, 3));
         }
         fetchData();
     }, []);
@@ -33,66 +34,10 @@ export const PageTracker = () => {
     return (
         <div>
             <h1>Tracker page</h1>
-
             <h2>Keeping track of Andy + Joe&apos;s github work</h2>
 
-            {andyData && (
-                <>
-                    <h2>Andy Github Data</h2>
-                    <ul>
-                        {andyData.map((commit) => {
-                            // eslint-disable-next-line camelcase
-                            const { id, repo, payload, created_at } = commit;
-
-                            return (
-                                <li key={id}>
-                                    <p>Repo: {repo.name}</p>
-                                    <p>
-                                        Commit:{' '}
-                                        <a
-                                            href={`https://github.com/${repo.name}/commit/${payload.commits[0].sha}`}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                        >
-                                            {payload.commits[0].message}
-                                        </a>
-                                    </p>
-                                    <p>Date: {format(new Date(created_at), 'do MMMM yyyy k:mm')}</p>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </>
-            )}
-
-            {joeData && (
-                <>
-                    <h2>Joe Github Data</h2>
-                    <ul>
-                        {joeData.map((commit) => {
-                            // eslint-disable-next-line camelcase
-                            const { id, repo, payload, created_at } = commit;
-
-                            return (
-                                <li key={id}>
-                                    <p>Repo: {repo.name}</p>
-                                    <p>
-                                        Commit:{' '}
-                                        <a
-                                            href={`https://github.com/${repo.name}/commit/${payload.commits[0].sha}`}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                        >
-                                            {payload.commits[0].message}
-                                        </a>
-                                    </p>
-                                    <p>Date: {format(new Date(created_at), 'do MMMM yyyy k:mm')}</p>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </>
-            )}
+            {andyData && <Tracker details={andyData} name="Andy" />}
+            {huntData && <Tracker details={huntData} name="Joe" />}
         </div>
     );
 };
