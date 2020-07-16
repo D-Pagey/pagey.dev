f2603109c9f49f1255e723f6dd1bf03a4b790662
Dan Page <dan.page91@gmail.com>
2020-07-16 21:05:18 +0100

    fix(checklist): add ids to keep track of which inputs are checked

--- src/components/PageChecklist/data.js
+++ src/components/PageChecklist/data.js
@@ -1,65 +1,131 @@
 export default [
     {
         title: 'Building the UI natively',
-        topics: ['strong semantic HTML', 'strong layout CSS flexbox + grid']
+        topics: [
+            {
+                id: 'a',
+                text: 'strong semantic HTML'
+            },
+            {
+                id: 'b',
+                text: 'strong layout CSS flexbox + grid'
+            }
+        ]
     },
     {
         title: 'Getting Data',
         topics: [
-            'Understand how APIs work',
-            'basic HTTP codes: 404, 403, 200',
-            'GET, POST, PUT, DELETE',
-            'understand various authenticating APIs',
-            'understanding async JS works (promises)',
-            'error handling'
+            { id: 'c', text: 'Understand how APIs work' },
+            { id: 'd', text: 'basic HTTP codes: 404, 403, 200' },
+            { id: 'e', text: 'GET, POST, PUT, DELETE' },
+            { id: 'f', text: 'understand various authenticating APIs' },
+            { id: 'g', text: 'understanding async JS works (promises)' },
+            { id: 'h', text: 'error handling' }
         ]
     },
     {
         title: 'React',
         topics: [
-            'Props & state',
-            'components: composition over inheritance',
-            'hooks',
-            'global state management',
-            'reducer / action pattern (Redux)',
-            'basic lifecycle (ComponentDidMount, old state)',
-            'CSS in JS / styled components'
+            { id: 'i', text: 'Props & state' },
+            { id: 'j', text: 'components: composition over inheritance' },
+            { id: 'k', text: 'hooks' },
+            { id: 'l', text: 'global state management' },
+            { id: 'm', text: 'reducer / action pattern (Redux)' },
+            { id: 'n', text: 'basic lifecycle (ComponentDidMount, old state)' },
+            { id: 'o', text: 'CSS in JS / styled components' }
         ]
     },
     {
         title: 'Testing',
         topics: [
-            'React Testing Library',
-            'Mocking',
-            'methodology: implementation details vs as user',
-            'integration testing vs unit vs e2e'
+            {
+                id: 'p',
+                text: 'React Testing Library'
+            },
+            {
+                id: 'q',
+                text: 'Mocking'
+            },
+            {
+                id: 'r',
+                text: 'methodology: implementation details vs as user'
+            },
+            {
+                id: 's',
+                text: 'integration testing vs unit vs e2e'
+            }
         ]
     },
     {
         title: 'JavaScript',
         topics: [
-            'array manipulation - codewars',
-            'object manipulation - codewars',
-            'map/filter/[reduce] - for loops',
-            'ES6: arrow funds, destructuring etc'
+            {
+                id: 't',
+                text: 'array manipulation - codewars'
+            },
+            {
+                id: 'u',
+                text: 'object manipulation - codewars'
+            },
+            {
+                id: 'v',
+                text: 'map/filter/[reduce] - for loops'
+            },
+            {
+                id: 'w',
+                text: 'ES6: arrow funds, destructuring etc'
+            }
         ]
     },
     {
         title: 'Git',
         topics: [
-            'git add, git commit, git fetch, git pull',
-            'updated a branch - git rebase, git merge',
-            'branching',
-            'PRs',
-            'Forking'
+            {
+                id: 'x',
+                text: 'git add, git commit, git fetch, git pull'
+            },
+            {
+                id: 'y',
+                text: 'updated a branch - git rebase, git merge'
+            },
+            {
+                id: 'z',
+                text: 'branching'
+            },
+            {
+                id: 'aa',
+                text: 'PRs'
+            },
+            {
+                id: 'ab',
+                text: 'Forking'
+            }
         ]
     },
     {
         title: 'Basic Fullstack',
-        topics: ['Manage a database', 'Serverless funcs']
+        topics: [
+            {
+                id: 'ac',
+                text: 'Manage a database'
+            },
+            {
+                id: 'ad',
+                text: 'Serverless funcs'
+            }
+        ]
     },
     {
         title: 'Misc',
-        topics: ['Storybook', 'Command Line']
+        topics: [
+            {
+                id: 'ad',
+                text: 'Storybook'
+            },
+            {
+                id: 'ae',
+                text: 'Command Line'
+            }
+        ]
     }
 ];
--- src/components/PageChecklist/index.js
+++ src/components/PageChecklist/index.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import data from './data';
 import * as S from './styles';
 
-export const PageChecklist = () => (
-    <div>
-        <h1>React Developer Checklist</h1>
+export const PageChecklist = () => {
+    const [completedTopics, setCompletedTopics] = useState({});
 
-        <S.Text>
-            An opinionated checklist of topics that one should be familiar with when aspiring to be a Front End
-            Developer. This is to be used to form the basis of a focussed learning curriculum.
-        </S.Text>
+    const handleChange = (id) => () =>
+        setCompletedTopics({
+            ...completedTopics,
+            [id]: !completedTopics[id]
+        });
 
-        {data.map((item) => (
-            <React.Fragment key={item.title}>
-                <h2>{item.title}</h2>
-                <S.List>
-                    {item.topics.map((topic) => (
-                        <li key={topic}>
-                            <S.Checkbox />
-                            {topic}
-                        </li>
-                    ))}
-                </S.List>
-            </React.Fragment>
-        ))}
-    </div>
-);
+    return (
+        <div>
+            <h1>React Developer Checklist</h1>
+
+            <S.Text>
+                An opinionated checklist of topics that one should be familiar with when aspiring to be a Front End
+                Developer. This is to be used to form the basis of a focussed learning curriculum.
+            </S.Text>
+
+            {data.map((item) => (
+                <React.Fragment key={item.title}>
+                    <h2>{item.title}</h2>
+                    <S.List>
+                        {item.topics.map((topic) => (
+                            <li key={topic.id}>
+                                <S.Checkbox onChange={handleChange(topic.id)} checked={!!completedTopics[topic.id]} />
+                                {topic.text}
+                            </li>
+                        ))}
+                    </S.List>
+                </React.Fragment>
+            ))}
+        </div>
+    );
+};
