421ecb75a5e4bc079083dee60ba5cbd0e0484c9c
Dan Page <dan.page91@gmail.com>
2020-08-17 21:31:30 +0100

    fix(header): remove checklist link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,6 @@ const Header = () => (
         <div>
             <S.Link to="/cv">cv</S.Link>
             <S.Link to="/tracker">tracker</S.Link>
-            <S.Link to="/checklist">checklist</S.Link>
         </div>
     </S.Header>
 );
