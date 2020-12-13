97851568fbfef73020b995d3635ebbb00b2dbc75
Dan Page <dan.page91@gmail.com>
2020-12-13 15:51:18 +0000

    build(dependencies): update all majors, minors and pathces

--- src/components/Pills/index.js
+++ src/components/Pills/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { arrayOf, string } from 'prop-types';
 import * as S from './styles';
 
-const Pills = ({ items, margin }) => {
-    return (
-        <S.List margin={margin}>
-            {items.map((item) => (
-                <S.Pill key={item}>{item}</S.Pill>
-            ))}
-        </S.List>
-    );
-};
+const Pills = ({ items, margin }) => (
+    <S.List margin={margin}>
+        {items.map((item) => (
+            <S.Pill key={item}>{item}</S.Pill>
+        ))}
+    </S.List>
+);
 
 Pills.propTypes = {
     items: arrayOf(string).isRequired,
